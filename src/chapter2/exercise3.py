3bdab74a1afaebbb4290f991b081285d04ee75a3
Your Name <you@example.com>
2021-03-25 22:55:54 +0300

    exercises in chapter2

--- src/chapter2/exercise3.py
+++ src/chapter2/exercise3.py
@@ -1,5 +1,5 @@
 # Program to calculate the gross pay of a worker
 hours_worked = input("Enter the hours worked:\n")
-rate = input("Enter the payment rate: ")
+rate = input("Enter the payment rate:\n ")
 pay = int(hours_worked)*float(rate)
 print(pay)
