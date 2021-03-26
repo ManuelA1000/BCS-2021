667488de9fcc15e3a647edaf9674b5c6f0bbdd20
Your Name <you@example.com>
2021-03-26 09:55:18 +0300

    exercises in chapter2

---
+++ src/chapter2/exercise6.py
@@ -0,0 +1,7 @@
+x1 = int(input('Enter x1:\n'))
+x2 = int(input('Enter x2:\n'))
+y1 = int(input('Enter y1:\n'))
+y2 = int(input('Enter y2:\n'))
+distance = ((((x2-x1)**2) + ((y2-y1)**2))**0.5)
+
+print('The distance between the two points is', distance)
