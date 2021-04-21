aea2ca3ff02e2a64f8ed1ba74aa37090080cd055
Your Name <you@example.com>
2021-04-21 20:42:46 +0300

    assignment5

---
+++ src/chapter5/exercise3.py
@@ -0,0 +1,6 @@
+
+
+print("Welcome to the vending machine change maker program \nChange maker initialized.\nStock contains:\n")
+stock = ["25 nickels", "25 dimes", "25 quarters", "0 ones", "0 fives"]
+for i in stock:
+    print(i)
