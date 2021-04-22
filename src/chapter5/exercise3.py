7d8eee87886983ed8b999529b97614c77a47fd0f
Your Name <you@example.com>
2021-04-22 12:37:46 +0300

    modified previous code, combined steps a and b that is asking for user input and checking if it is legal, quitting when q is hit

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -1,26 +1,18 @@
-# vending machine change maker program
-a = ["25 nickles", "25 dimes", "25 quarters", "0 ones ", "0 fives"]  # a list of stock
-print("Welcome to the vending machine change maker program:")
-print("Change maker initialized:")
-for i in a:
-    print(i)
+# vending machine change maker programme.
+print("Welcome to the vending machine change maker programme")
+print("Change maker initialized")
+print("Stock contains:")
+print("25 nickels\n25 dimes\n25 quarters\n0 ones\n0 fives")
+
+# requesting for price from the user
 while True:
-    price = input("Enter the purchase price (xx.xx) or `q' to quit:\n")
-    try:
-        price = float(price)
-        if price < 0 and price % 0.05 != 0:
-            print("Illegal price: Must be a non-negative multiple of 5 cents.")
-        else:
-            print("Menu for deposits")
-            menu = ["'n' - deposit a nickel", "'d' - deposit a dime", "'q' - deposit a quarter",
-                    "'o' - deposit a one dollar bill", "'f' - deposit a five dollar bill", "'c' - cancel the purchase"]
-            for deposit in menu:
-                print(deposit)
-    except:
-        price = "q"
 
-
-
-
-
-
+    # requesting for price input
+    price = input("Enter the purchase price (xx.xx) or `q' to quit:")
+    if price == "q":
+        break
+    item_cost = float(price)
+    if (item_cost > 0) and round((item_cost*100) % 5 == 0):
+        continue
+    else:
+        print("Illegal price: Must be a non-negative multiple of 5 cents.")
