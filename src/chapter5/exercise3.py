037362c222d26ce3e97c7d4907f672752f543a29
group1BCS1 <ashabahebwaabdulswabur29@gmail.com>
2021-04-22 02:47:47 -0700

    Update exercise3.py
    commit

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -1,3 +1,9 @@
+## group1BCS1 MEMBERS
+#1.ASHABAHEBWA ABDULSWABUR  2020/BCS/023/PS
+#2. ACAR EMMANUEL 2020/BCS/011/PS
+#3. OGONGA CHRISTOPHER 2020/BCS/008
+#4. MUJUNI SHEM 2020/BCS/083/PS
+#5. SSEMPEWO ROGERS 2020/BCS/077/PS
 # vending machine change maker program
 a = ["25 nickles" ,"25 dimes" , "25 quarters" , "0 ones " , "0 fives"] # a list of stock 
 print("Welcome to the vending machine change maker program:") ;print("Change maker initialized:")
