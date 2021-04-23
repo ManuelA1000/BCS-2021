e3b835eb621e0e76c9e191572886b8beb26442e7
Your Name <you@example.com>
2021-04-23 07:47:17 +0300

    status: 70% done, still figuring out a few

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -7,6 +7,40 @@ def selection_menu():
           "\n'c' - cancel the purchase")
 
 
+def amount_due(price):
+    total_cents = round(price*100)
+    dollars = total_cents // 100
+    cents = total_cents % 100
+    print("Payment due: ", dollars, "dollars and ", cents, "cents")
+
+
+def balance(amount_deposited, item_cost):
+    print("Take the change below:")
+    change = amount_deposited-item_cost
+    change = round(change*100)
+    while change >= 25:  
+        print(change//25, "quarter(s)")
+        change = change % 25
+        continue
+    while change >= 10: 
+        print(change//10, "dime(s)")
+        change = change % 10
+    while change >= 5:  
+        print(change//5, "nickel(s)")
+        break
+    else: 
+        print("0 cents")
+
+
+def enter_deposit():
+    amount_due(item_cost)
+    amount_deposited = 0
+    while item_cost > amount_deposited:
+        deposit = input("Indicate your deposit")
+        if deposit == "c":
+            break
+
+    
 # main programme starts off here
 print("Welcome to the vending machine change maker programme")
 print("Change maker initialized")
