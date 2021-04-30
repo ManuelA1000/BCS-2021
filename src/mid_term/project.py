ad1c93873c91a5b72feb403b438f4d7f6cd332b0
Your Name <you@example.com>
2021-04-30 22:48:38 +0300

    simple version of the program

--- src/mid_term/project.py
+++ src/mid_term/project.py
@@ -2,3 +2,8 @@
 # main program starts here
 while True:
     customer_code = input("Enter customer code:\n")
+    initial_reading = input("Enter the beginning meter reading:\n")
+    ending_reading = input("Enter the ending meter reading:\n")
+    print("Customer code:", customer_code)
+    print("Beginning meter reading:", initial_reading)
+    print("Ending meter reading:", ending_reading)
