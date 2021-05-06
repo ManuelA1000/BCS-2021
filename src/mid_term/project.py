9ff569f48ce727a06fb9d37e7bc56051adc096a8
Your Name <you@example.com>
2021-05-06 19:04:20 +0300

    final working code,traps negative values, values with more than ten digits and characters entered instead of numerical values for meter readings

--- src/mid_term/project.py
+++ src/mid_term/project.py
@@ -14,49 +14,58 @@ def gallons_used(a, b):
 while True:
     customer_code = input("Enter customer code:\n")
     if customer_code == "r" or customer_code == "R":
-        initial_reading = int(input("Enter the beginning meter reading:\n"))
-        ending_reading = int(input("Enter the ending meter reading:\n"))
-        if 0 <= initial_reading <= 999999999 and 0 <= ending_reading <= 999999999:
-            print(f"Customer code: {customer_code}")
-            print(f"Beginning meter reading: {initial_reading:09d}")
-            print(f"Ending meter reading:{ending_reading:09d}")
-            gallons = gallons_used(initial_reading, ending_reading)
-            bill = 5 + (0.0005 * gallons)
-            print(f"Amount billed: ${bill:.2f}")
-        else:
-            print("Invalid entry")
+        try:
+            initial_reading = int(input("Enter the beginning meter reading:\n"))
+            ending_reading = int(input("Enter the ending meter reading:\n"))
+            if 0 <= initial_reading <= 999999999 and 0 <= ending_reading <= 999999999:
+                print(f"Customer code: {customer_code}")
+                print(f"Beginning meter reading: {initial_reading:09d}")
+                print(f"Ending meter reading:{ending_reading:09d}")
+                gallons = gallons_used(initial_reading, ending_reading)
+                bill = 5 + (0.0005 * gallons)
+                print(f"Amount billed: ${bill:.2f}")
+            else:
+                print("Invalid entry")
+        except:
+            print("Enter a numerical value")
     elif customer_code == "c" or customer_code == "C":
-        initial_reading = int(input("Enter the beginning meter reading:\n"))
-        ending_reading = int(input("Enter the ending meter reading:\n"))
-        if 0 <= initial_reading <= 999999999 and 0 <= ending_reading <= 999999999:
-            print(f"Customer code: {customer_code}")
-            print(f"Beginning meter reading: {initial_reading:09d}")
-            print(f"Ending meter reading:{ending_reading:09d}")
-            gallons = gallons_used(initial_reading, ending_reading)
-            if gallons <= 4000000:
-                bill = 1000
+        try:
+            initial_reading = int(input("Enter the beginning meter reading:\n"))
+            ending_reading = int(input("Enter the ending meter reading:\n"))
+            if 0 <= initial_reading <= 999999999 and 0 <= ending_reading <= 999999999:
+                print(f"Customer code: {customer_code}")
+                print(f"Beginning meter reading: {initial_reading:09d}")
+                print(f"Ending meter reading:{ending_reading:09d}")
+                gallons = gallons_used(initial_reading, ending_reading)
+                if gallons <= 4000000:
+                    bill = 1000
+                else:
+                    bill = 1000 + (gallons - 4000000) * 0.00025
+                print(f"Amount billed: ${bill:.2f}")
             else:
-                bill = 1000 + (gallons - 4000000) * 0.00025
-            print(f"Amount billed: ${bill:.2f}")
-        else:
-            print("Invalid entry")
+                print("Invalid entry")
+        except:
+            print("Enter a numerical value")
     elif customer_code == "i" or customer_code == "I":
-        initial_reading = int(input("Enter the beginning meter reading:\n"))
-        ending_reading = int(input("Enter the ending meter reading:\n"))
-        if 0 <= initial_reading <= 99999999 and 0 <= ending_reading <= 999999999:
-            print(f"Customer code: {customer_code}")
-            print(f"Beginning meter reading: {initial_reading:09d}")
-            print(f"Ending meter reading:{ending_reading:09d}")
-            gallons = gallons_used(initial_reading, ending_reading)
-            if gallons <= 4000000:
-                bill = 1000
-            elif 4000000 < gallons <= 10000000:
-                bill = 2000
+        try:
+            initial_reading = int(input("Enter the beginning meter reading:\n"))
+            ending_reading = int(input("Enter the ending meter reading:\n"))
+            if 0 <= initial_reading <= 99999999 and 0 <= ending_reading <= 999999999:
+                print(f"Customer code: {customer_code}")
+                print(f"Beginning meter reading: {initial_reading:09d}")
+                print(f"Ending meter reading:{ending_reading:09d}")
+                gallons = gallons_used(initial_reading, ending_reading)
+                if gallons <= 4000000:
+                    bill = 1000
+                elif 4000000 < gallons <= 10000000:
+                    bill = 2000
+                else:
+                    bill = 2000 + (gallons - 10000000) * 0.00025
+                print(f"Amount billed: ${bill:.2f}")
             else:
-                bill = 2000 + (gallons - 10000000) * 0.00025
-            print(f"Amount billed: ${bill:.2f}")
-        else:
-            print("Invalid entry")
+                print("Invalid entry")
+        except:
+            print("Enter a numerical value")
     else:
         print("Invalid customer code")
         break
