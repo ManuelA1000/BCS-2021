983468bd30cb5dc31c9bb430629f95cba1c2b253
group1BCS1 <ashabahebwaabdulswabur29@gmail.com>
2021-05-19 23:54:34 +0300

    commit changes

---
+++ src/project02/project2_b.py
@@ -0,0 +1,7 @@
+while True:
+    fhandle = input("Enter the name of the input file:\n")
+    fhandle = open(fhandle)
+    year = input("Enter year:\n")
+    income_levels = [1, 2 , 3 , 4]
+    income_level = input("Enter income level:")
+
