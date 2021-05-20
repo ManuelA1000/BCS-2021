5edf27d85a2c238618ddc9c2c5c3ae483c44cedc
Your Name <you@example.com>
2021-05-20 10:35:26 +0300

    halfway done

--- src/project02/project2_a.py
+++ src/project02/project2_a.py
@@ -1,15 +1,3 @@
-# we shall start by openning the file that contains the text that we want to use
-with open('measles.txt' , 'r') as f:
-    year = input("Enter year:\n")
-for line in f:
-        line = line.upper()
-        year = line[-5:]
-        if year.startswith(year):
-            print(line)
-        elif year == "all" or year == "ALL" or year == " ":
-            print(f)   
-        else:
-            print("invalid input , try again later")
 
 try:
     with open('measles.txt', 'r') as f:
@@ -35,7 +23,3 @@ try:
 except FileNotFoundError:
     print("File not found")
     quit()
-
-
-             
-    
