989cefc5f386e082ae9d3bbb4a28ab9432855481
Your Name <you@example.com>
2021-05-21 22:59:56 +0300

    final work

--- src/project02/project2_a.py
+++ src/project02/project2_a.py
@@ -1,20 +1,18 @@
-
+# program reading portions of a file based on year entered
 try:
-    with open('measles.txt', 'r') as f:
+    with open('measles.txt', 'r') as f:  # opening file to read from
         output = input("Please enter the name of the output file:\n")
         year = input("Enter a year:\n")
         try:
-            if len(year) <= 4 or year == 'all' or year == 'ALL' or year == (int(year)):
+            if len(year) <= 4:
                 pass
             else:
                 quit("Year must be less than four characters or less characters or an integer")
         except ValueError:
             quit("Enter valid year.Thank you.")
         with open(output, 'w') as output:
-            u = f.readlines()
-            for line in u:
-
-                if (line[-1] == year) or (line[-2] == year) or (line[-3] == year) or (line[-4] == year):
+            for line in f:
+                if line[88:92].startswith(year):
                     output.write(line)
                 elif year == line[:] or year == "all" or year == "ALL":
                     output.write(line)
